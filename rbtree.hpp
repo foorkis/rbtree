2d392ac91667de4f28b60e323bed10f0f34196aa
Georgiy Stolnikov <foorkis@gmail.com>
2016-04-01 12:31:33 +0300

    it's a kind of magic

--- rbtree.hpp
+++ rbtree.hpp
@@ -5,31 +5,40 @@
 using std::cin;
 using std::cout;
 
+enum Color {RED, BLACK};
+
 template <class T>
 struct Node
 {
     T data;
-    char color;
+    Color color;
     Node* left;
     Node* right;
     Node* parent;
-    Node ()
-    	: color('b')
-    	, left(nullptr)
-    	, right(nullptr)
-    	, parent(nullptr){};
-    Node (T value, char c)
+    Node ()              // for NIL node
+    	: color(BLACK)
+    	, left(this)
+    	, right(this)
+    	, parent(this){};
+    Node (T value, Color c)
     	: data(value)
     	, color(c)
     	, left(nullptr)
     	, right(nullptr)
     	, parent(nullptr){};
-    Node (T value, char c, Node* p)
+    Node (T value, Color c, Node* p)
     	: data(value)
     	, color(c)
     	, left(nullptr)
     	, right(nullptr)
     	, parent(p){};
+    void printNode()
+	{
+	    if (RED==color)
+		cout << "(RED|" << data << ")";
+	    else
+		cout << "(BLACK|" << data << ")";
+	}
 };
 
 template <class T>
@@ -42,6 +51,9 @@ public:
     RBtree()
 	{
 	    nil=new Node<T>();
+	    nil->left=nil;
+	    nil->right=nil;
+	    nil->parent=nil;
 	    _root=nil;
 	};
     RBtree(T value)
@@ -55,10 +67,20 @@ public:
 	    if (_root->left!=nullptr)
 		delete _root->left;
 	};
-    
-    bool getColor();
-    void setColor(Color c);
-    void flipColor();
+    Color getColor()
+	{
+	    return _root->color;
+	};
+    void setColor(Color c)
+	{
+	    _root->color=c;
+	};
+    void flipColor()
+	{
+	    (_root->color=RED)
+		? _root->color=BLACK
+		: _root->color=RED;
+	};
     void insert(T value)
 	{
 	    
